c27460fd53facb1698022beb1ad38eeb06042851
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2018-10-24 15:31:27 +0200

    Try using the ant design language

--- ui/src/index.js
+++ ui/src/index.js
@@ -5,6 +5,8 @@ import registerServiceWorker from './registerServiceWorker';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
+import "antd/dist/antd.css"
+
 const client = new ApolloClient({
     uri: process.env.REACT_APP_GRAPHQL_URI
 })
