c1d25fe6b68c503c54e98f3bf9f7958e26964477
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2018-10-26 10:31:21 +0200

    Clean up TextList to use antd table presentation and begin link

--- ui/src/components/App.js
+++ ui/src/components/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import TextItem from './TextItem'
-
-// Layout 
 import { Layout } from 'antd';
+import TextItem from './TextItem'
+import TextList from './TextList';
+
 const { Content } = Layout;
 
 class App extends Component {
@@ -12,7 +12,8 @@ class App extends Component {
           <Content style={{ padding: '0 50px' }}>
             <div style={{ background: '#fff', padding: 12, minHeight: 280 }}>
               <h1>Welcome to Commentaria Aristotelica</h1>
-              <TextItem item="180"/>
+              <TextItem item="86"/>
+              <TextList />
             </div>
           </Content>
         </Layout>
--- ui/src/components/TextList.js
+++ ui/src/components/TextList.js
@@ -1,17 +1,8 @@
 import React from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-import { withStyles } from "@material-ui/core/styles";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  Tooltip,
-  Paper,
-  TableSortLabel
-} from "@material-ui/core";
+import { Table } from 'antd'
+
 
 const styles = theme => ({
   root: {
@@ -49,7 +40,22 @@ class TextList extends React.Component {
   };
 
   render() {
-    const { order, orderBy } = this.state;
+    const columns = [
+      {
+        title: 'Name',
+        dataIndex: 'authorName',
+      },
+      {
+        title: 'Title',
+        dataIndex: 'title',
+        render: (text, record) => <a href={record.text_id}>{text}</a>
+      },
+      {
+        title: 'ID',
+        dataIndex: 'text_id',
+      }
+    ]
+
     return (
       <Query
         query={gql`
@@ -61,7 +67,7 @@ class TextList extends React.Component {
             Text(first: $first, offset: $offset, orderBy: $orderBy) {
               authorName
               title
-              _id
+              text_id
             }
           }
         `}
@@ -76,82 +82,7 @@ class TextList extends React.Component {
           if (error) return <p>Error</p>;
 
           return (
-            <Paper className={this.props.classes.root}>
-              <Table className={this.props.classes.table}>
-                <TableHead>
-                  <TableRow>
-                    <TableCell
-                      key="authorName"
-                      sortDirection={orderBy === "title" ? order : false}
-                    >
-                      <Tooltip
-                        title="Sort"
-                        placement="bottom-start"
-                        enterDelay={300}
-                      >
-                        <TableSortLabel
-                          active={orderBy === "authorName"}
-                          direction={order}
-                          onClick={() => this.handleSortRequest("authorName")}
-                        >
-                          Author
-                        </TableSortLabel>
-                      </Tooltip>
-                    </TableCell>
-                    <TableCell
-                      key="title"
-                      sortDirection={orderBy === "title" ? order : false}
-                    >
-                      <Tooltip
-                        title="Sort"
-                        placement="bottom-end"
-                        enterDelay={300}
-                      >
-                        <TableSortLabel
-                          active={orderBy === "title"}
-                          direction={order}
-                          onClick={() => this.handleSortRequest("title")}
-                        >
-                          Text title
-                        </TableSortLabel>
-                      </Tooltip>
-                    </TableCell>
-                    <TableCell
-                      key="_id"
-                      sortDirection={orderBy === "_id" ? order : false}
-                      numeric
-                    >
-                      <Tooltip
-                        title="Sort"
-                        placement="bottom-start"
-                        enterDelay={300}
-                      >
-                        <TableSortLabel
-                          active={orderBy === "_id"}
-                          direction={order}
-                          onClick={() => this.handleSortRequest("_id")}
-                        >
-                          Text id
-                        </TableSortLabel>
-                      </Tooltip>
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {data.Text.map(n => {
-                    return (
-                      <TableRow key={n._id}>
-                        <TableCell component="th" scope="row">
-                          {n.authorName}
-                        </TableCell>
-                        <TableCell>{n.title}</TableCell>
-                        <TableCell numeric>{n._id}</TableCell>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            </Paper>
+            <Table dataSource={data.Text} columns={columns} rowKey="text_id" />
           );
         }}
       </Query>
@@ -159,4 +90,4 @@ class TextList extends React.Component {
   }
 }
 
-export default withStyles(styles)(TextList);
+export default TextList;
