cffd05077d8fb5149ad455cb9168c8b8e51fe25f
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2018-11-21 13:40:45 +0100

    Update the text list page and get a basic table working

--- ui/src/components/TextList.js
+++ ui/src/components/TextList.js
@@ -3,15 +3,30 @@ import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import { Table } from 'antd'
 
+const TEXTS_QUERY = gql`
+  query allTexts {
+    Text {
+      id
+      title
+      attributions {
+        id
+        person {
+          name
+          id
+        }
+      }
+    }
+  }
+`
+
 class TextList extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
       order: "asc",
-      orderBy: "authorName",
+      orderBy: "title",
       page: 0,
-      rowsPerPage: 10
     };
   }
 
@@ -26,50 +41,42 @@ class TextList extends React.Component {
     this.setState({ order, orderBy });
   };
 
-  render() {
-    const columns = [
-      {
-        title: 'Name',
-        dataIndex: 'authorName',
-      },
-      {
-        title: 'Title',
-        dataIndex: 'title',
-        render: (text, record) => <a href={`detail/${record.text_id}`}>{text}</a>
-      },
-      {
-        title: 'ID',
-        dataIndex: 'text_id',
-      }
-    ]
+  onShowSizeChange = (current, pageSize) => {
+    console.log(current, pageSize);
+  }
 
+  render() {
     return (
-      <Query
-        query={gql`
-          query usersPaginateQuery(
-            $first: Int
-            $offset: Int
-            $orderBy: _TextOrdering
-          ) {
-            Text(first: $first, offset: $offset, orderBy: $orderBy) {
-              authorName
-              title
-              text_id
-            }
-          }
-        `}
-        variables={{
-          first: this.state.rowsPerPage,
-          offset: this.state.rowsPerPage * this.state.page,
-          orderBy: this.state.orderBy + "_" + this.state.order
-        }}
-      >
+      <Query query={TEXTS_QUERY}>
         {({ loading, error, data }) => {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error</p>;
 
+          const texts = data.Text.map(text => ({
+            id: text.id,
+            title: text.title,
+            authors: text.attributions ? text.attributions.map(a => a.person.name).join(' / ') : 'No name'
+          }))
+          const columns = [
+            {
+              title: 'Authors',
+              dataIndex: 'authors',
+            },
+            {
+              title: 'Title',
+              dataIndex: 'title',
+              render: (text, record) => <a href={`detail/${record.id}`}>{text}</a>
+            },
+            {
+              title: 'ID',
+              dataIndex: 'id',
+            }
+          ]
+
           return (
-            <Table dataSource={data.Text} columns={columns} rowKey="text_id" />
+            <Table
+              pagination={{ pageSize: 10, showSizeChanger: true, }}
+              dataSource={texts} columns={columns} rowKey="id" />
           );
         }}
       </Query>
