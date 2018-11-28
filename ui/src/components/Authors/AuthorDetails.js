d81c1c709bed76c5f11223205426f837d1f9c224
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2018-11-28 22:14:35 +0100

    Iterate on the display and content of the author detail module

--- ui/src/components/Authors/AuthorDetails.js
+++ ui/src/components/Authors/AuthorDetails.js
@@ -2,16 +2,23 @@ import React, { Component } from "react";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
 import { Link, withRouter } from "react-router-dom";
-import { List, Button, Table } from "antd";
+import { Button, Table } from "antd";
 import DescriptionList from "../DescriptionList";
-import { normCertainty, formatDates } from '../utils';
+import { normCertainty } from '../utils';
 
 
 const AUTHOR_QUERY = gql`
   query authorInfo($id: ID!) {
     Person(id: $id) {
       id
-      name
+      description
+      note
+      biography
+      names {
+        id
+        value
+        language
+      }
       attributions {
         id
         note
@@ -44,6 +51,8 @@ class AuthorDetails extends Component {
 
   displayingDetails = (id) => this.state.visibleDetails.includes(id)
 
+  showPagination = (records) => records.length > 10
+
   render() {
     const urlParams = this.props.match.params
 
@@ -58,18 +67,6 @@ class AuthorDetails extends Component {
             return <div>Error: There is no author with the id {urlParams.id}</div>
           }
 
-          const titleColumns = [
-            { title: 'Title', dataIndex: 'title' },
-            { title: 'Certainty', dataIndex: 'certainty' }
-          ]
-          const titleData = author.attributions.map(attribution => ({
-            id: attribution.id,
-            title: attribution.text.title,
-            note: attribution.note,
-            source: attribution.source,
-            certainty: normCertainty(attribution.certainty),
-          }))
-
           return (
             <React.Fragment>
               <h1>Author details</h1>
@@ -77,17 +74,50 @@ class AuthorDetails extends Component {
                 <Link to={"/author/edit/" + author.id}>Edit author</Link>
               </Button>
               <section>
-                <h2>Name</h2>
+                <h2>Names</h2>
+                <Table
+                  columns={[
+                    { title: 'Name', dataIndex: 'value' },
+                    { title: 'Language', dataIndex: 'language' }
+                  ]}
+                  dataSource={author.names.map(name => ({
+                    key: name.id,
+                    value: name.value,
+                    language: name.language
+                  }))}
+                  size={'small'}
+                  pagination={this.showPagination(author.names)}
+                />
+              </section>
+              <section>
+                <h2>Short description</h2>
+                <div>
+                  {author.description}
+                </div>
               </section>
               <section>
                 <h2>Texts</h2>
                 <Table
-                  columns={titleColumns}
-                  dataSource={titleData}
-                  rowKey="id"
+                  columns={[
+                    { title: 'Title', dataIndex: 'title' },
+                  ]}
+                  dataSource={author.attributions.map(attribution => ({
+                    key: attribution.id,
+                    title: attribution.text.title,
+                    note: attribution.note,
+                    source: attribution.source,
+                    certainty: normCertainty(attribution.certainty),
+                  }))}
+                  size={'small'}
+                  pagination={this.showPagination(author.attributions)}
                   expandedRowRender={record => (
                     <DescriptionList
                       items={[
+                        {
+                          title: 'Certainty',
+                          description: record.certainty,
+                          key: record.id + '_certainty'
+                        },
                         {
                           title: 'Note',
                           description: record.note || undefined,
@@ -102,6 +132,24 @@ class AuthorDetails extends Component {
                     />
                   )}
                 />
+              </section>
+              <section>
+                <h2>Biography</h2>
+                <div>
+                  {author.biography}
+                </div>
+              </section>
+              <section>
+                <h2>Note</h2>
+                <div>
+                  {author.note}
+                </div>
+              </section>
+              <section>
+                <h2>Resources</h2>
+              </section>
+              <section>
+                <h2>Literature</h2>
               </section>
             </React.Fragment>
           )
