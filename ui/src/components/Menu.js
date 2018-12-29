cab1290e81196be020a97fa11f0412b4a7b1e144
Michael Stenskjær Christensen <michael.stenskjaer@gmail.com>
2018-12-29 18:13:14 +0100

    Update menu to stateless function and add item

--- ui/src/components/Menu.js
+++ ui/src/components/Menu.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import { withRouter } from 'react-router'
 import { Menu } from 'antd';
 
 const { Item } = Menu;
 
-class SideMenu extends Component {
-  render() {
-    return (
-      <Menu theme="light">
-        <Item key="1">
-          <Link to="/">
-            Main
-          </Link>
-        </Item>
-        <Item key="2">
-          <Link to="/texts">
-            Texts
-          </Link>
-        </Item>
-      </Menu>
-    );
-  }
+function SideMenu() {
+  return (
+    <Menu theme="light">
+      <Item key="home">
+        <Link to="/">
+          Main
+        </Link>
+      </Item>
+      <Item key="texts">
+        <Link to="/texts">
+          Texts
+        </Link>
+      </Item>
+      <Item key="authors">
+        <Link to="/authors">
+          Authors
+        </Link>
+      </Item>
+    </Menu>
+  );
 }
 
 export default withRouter(SideMenu);
